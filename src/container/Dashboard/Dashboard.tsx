93e329e5a5318cf8d476e88d6161463ba013af94
william <emelifonwuw@gmail.com>
2025-07-18 00:45:13 +0100

    `Update grid column count in Dashboard component to use lg:grid-cols-2 on larger screens`

--- src/container/Dashboard/Dashboard.tsx
+++ src/container/Dashboard/Dashboard.tsx
@@ -16,7 +16,7 @@ import {
       <main className="md:p-6 space-y-6">
         <DashboardStats />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
           <RecentActivity />
           <RecentComments />
         </div>
